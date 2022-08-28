8e4e55d0626ab32c74c21bbd5034ebd1913316d9
Nazmus Sayad <247sayad@gmail.com>
2022-08-28 20:45:19 +0600

    ):

--- src/index.js
+++ src/index.js
@@ -2,25 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
 import App from './App'
 import React from 'react'
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { hasError: false }
-  }
 
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true }
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>
-    }
-
-    return this.props.children
-  }
-}
 const rootElement = document.getElementById('Root')
 const root = createRoot(rootElement)
 
